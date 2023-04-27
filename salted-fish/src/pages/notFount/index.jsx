f562af4cd1ad26689dd796d7906c0fdf88da974a
zous <18310002219@163.com>
2023-04-27 16:48:52 +0800

    commit

---
+++ salted-fish/src/pages/notFount/index.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: zous 18310002219@163.com
+ * @Date: 2023-04-27 16:36:48
+ * @LastEditors: zous 18310002219@163.com
+ * @LastEditTime: 2023-04-27 16:48:00
+ * @FilePath: /react-learn-lesson/salted-fish/src/pages/notFount/index.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import './index.css'
+
+export default function notFound () {
+  return (
+    <div className="not-wrap">
+      <p>404 not found</p>
+    </div>
+  )
+}
