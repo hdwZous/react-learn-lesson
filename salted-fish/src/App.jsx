6b8c73a370263a57c6fcae3622c990e287293599
zous <18310002219@163.com>
2023-05-06 16:23:17 +0800

    commit

--- salted-fish/src/App.jsx
+++ salted-fish/src/App.jsx
@@ -2,7 +2,7 @@
  * @Author: zous 18310002219@163.com
  * @Date: 2023-04-27 11:09:30
  * @LastEditors: zous 18310002219@163.com
- * @LastEditTime: 2023-04-27 11:12:51
+ * @LastEditTime: 2023-05-06 16:14:16
  * @FilePath: /react-learn-lesson/salted-fish/src/App.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,7 +24,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + hdw</h1>
+      <h1 className='linerr'>Vite + React + hdw</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
